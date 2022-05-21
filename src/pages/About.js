12cce0c00cb546dfb6d23e5a318aee9d014d8783
Deanna K <keysdeanna@gmail.com>
2022-05-21 16:00:38 -0400

    add pages for future use

---
+++ src/pages/About.js
@@ -0,0 +1,5 @@
+const About = function (props){
+    return <h1>About</h1>
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = function (props){
+    return <h1>Home</h1>
+}
+
+export default Home
