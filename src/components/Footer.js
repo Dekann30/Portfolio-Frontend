ded197eabc0ee11cbc83d37764bd031ff51fb2e0
Deanna K <keysdeanna@gmail.com>
2022-05-21 15:58:24 -0400

    adds header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = function(props){
+    return <h1>Footer</h1>
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+
+const Header = function(props){
+    return <h1>Header</h1>
+}
+
+export default Header
