b9b1e2b4b43aa1f0a1d378363dca144dac68d25d
Deanna K <keysdeanna@gmail.com>
2022-05-28 19:27:48 -0400

    adds functioning About and Projects pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,26 @@
+import {useState, useEffect} from 'react'
+
 const About = function (props){
-    return <h1>About</h1>
+
+    const [about, setAbout] = useState(null)
+
+    const getAbout = async ()=>{
+        const response = await fetch(props.URL + 'about')
+        const data = await response.json()
+        setAbout(data)
+    }
+
+    useEffect(()=> {getAbout()},[])
+
+    const loaded = () =>(
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    )
+
+    return about ? loaded() : <h1>Loading...</h1>
 }
 
 export default About
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,33 @@
+import {useState, useEffect} from 'react'
+
 const Projects = function (props){
-    return <h1>Projects</h1>
+
+    const [projects, setProjects] = useState()
+
+    const getProjects = async () =>{
+        const response = await fetch(props.URL + 'projects')
+        const data = await response.json()
+        setProjects(data)
+    }
+
+    useEffect(()=>{getProjects()}, [])
+
+    const loaded = ()=>{
+        return (projects.map((project) =>(
+            <div>
+                <h1>{project.name}</h1>
+                <img src={project.image} alt={project.name} />
+                <a href={project.git}> 
+                    <button>Github</button>
+                </a>
+                <a href={project.live}>
+                    <button>Live Site</button>
+                </a>
+            </div>
+        )))
+    }
+
+    return projects ? loaded() : <h1>Loading...</h1>
 }
 
 export default Projects
