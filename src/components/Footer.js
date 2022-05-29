f3289598e5ee89ed16bdf0a382e1a6db39340924
Deanna K <keysdeanna@gmail.com>
2022-05-28 21:26:37 -0400

    adds stylized footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,26 @@
+import {Link} from "react-router-dom"
+
 const Footer = function(props){
-    return <h1>Footer</h1>
+    const footStyle={
+        display: "flex",
+        justifyContent: "space-around",
+        position: 'absolute',
+        bottom: "10px",
+        border: "3px solid black",
+        padding: "8px",
+        width: "90%",
+        margin: '0px 45.844px',
+    }
+
+    return(
+        <header>
+            <nav style={footStyle}>
+                <Link to='/'>Home</Link>
+                <Link to='/projects'>Projects</Link>
+                <Link to='/about'>About</Link>
+            </nav>
+        </header>
+    )
 }
 
 export default Footer
